3ce03c42f0531f0bde38d03f6ae3544d947a0cdc
jesse-rb <jesse.reynekebarnard@gmail.com>
2023-07-14 16:56:41 +1200

    fix nvim clipboard for wsl

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,24 +1,34 @@
+print('hello nvim')
 require('base')
 require('highlights')
 require('plugins')
 require('maps')
 
 local has = function(x)
-  return vim.fn.has(x) == 1
+    return vim.fn.has(x) == 1
 end
 
 local is_mac = has "macunix"
 local is_win = has "win32"
 local is_unix = has "unix"
+local is_wsl = has "wsl"
 
 if is_mac then
-  require('macos')
+    print('is mac')
+    require('macos')
 end
 
 if is_win then
-  require('windows')
+    print('is win')
+    require('windows')
 end
 
 if (is_unix) then
-  require('unix')
+    print('is unix')
+    require('unix')
+end
+
+if (is_wsl) then
+    print('is wsl')
+    require('wsl')
 end
