b3d3e67c1c9b673ca62449aa536bdc1c9cfd1753
jesse-rb <jesse.reynekebarnard@gmail.com>
2025-04-20 15:41:02 +1200

    add keymaps to help prevent accidently overwriting clipboard all the time

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Stop yanking stuff all the time!!
+map("n", "d", '"_d', { desc = "Delete without yanking!" })
+map("v", "d", '"_d', { desc = "Delete without yanking!" })
+
+map("n", "c", '"_c', { desc = "Change without yanking!" })
+map("v", "c", '"_c', { desc = "Change without yanking!" })
+
+map("v", "p", '"_dp', { desc = "Paste without yanking!" })
+
+map("n", "x", '"_x', { desc = "Delete character without yanking!" })
