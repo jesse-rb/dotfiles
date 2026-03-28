d6f2ae72f5ed8c9a36cf685ce2161da35bd7cbd6
jesse-rb <jesse.reynekebarnard@gmail.com>
2026-03-29 00:22:50 +1300

    Adjust mini statusline and add mini icons dep, and add noice and nui dep

--- .config/nvim/lua/jesse-rb/plugins/mini.statusline.lua
+++ .config/nvim/lua/jesse-rb/plugins/mini.statusline.lua
@@ -1,18 +1,31 @@
 return {
     "nvim-mini/mini.statusline",
     version = "*",
+    dependencies = {
+        "nvim-mini/mini.icons",
+    },
     opts = {
         content = {
             active = function()
+                local MiniIcons = require("mini.icons")
+
                 local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
                 local git = MiniStatusline.section_git({ trunc_width = 75 })
                 local filename = MiniStatusline.section_filename({ trunc_width = 140 })
                 local filetype = vim.bo.filetype
 
+                -- get file type icon from mini.icons
+                local filetype_icon, filetype_icon_hl =
+                    require("mini.icons").get("file", vim.api.nvim_buf_get_name(0))
+
                 return MiniStatusline.combine_groups({
                     { hl = mode_hl, strings = { mode } },
                     { hl = "MiniStatuslineDevinfo", strings = { git } },
-                    { hl = "MiniStatuslineFilename", strings = { filename, filetype } },
+                    { hl = "MiniStatuslineFilename", strings = { filename } },
+                    {
+                        hl = filetype_icon_hl or "MiniStatuslineFilename",
+                        strings = { filetype_icon .. " " .. filetype },
+                    },
                 })
             end,
         },
