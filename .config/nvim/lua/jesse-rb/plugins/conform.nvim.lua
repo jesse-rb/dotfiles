a61784725b7090d592a6516c6e32116ff8eeb92b
jesse-rb <jesse.reynekebarnard@gmail.com>
2026-04-28 22:38:18 +1200

    Add missing plguins

---
+++ .config/nvim/lua/jesse-rb/plugins/conform.nvim.lua
@@ -0,0 +1,19 @@
+return {
+    "stevearc/conform.nvim",
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            javascript = { "prettier" },
+            javascriptreact = { "prettier" },
+            typescript = { "prettier" },
+            typescriptreact = { "prettier" },
+            css = { "prettier" },
+            html = { "prettier" },
+            json = { "prettier" },
+        },
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_fallback = true, -- for using lsp formatters such as gopls
+        },
+    },
+}
